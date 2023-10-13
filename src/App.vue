72af74641bf9d655112544716ebb5448979984a7
damara <damarmuhamad13@gmail.com>
2023-10-13 18:34:58 +0700

    update(): fix-animate-in-project

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container mt-5">
     <h1>VueShop</h1>
     <!-- eslint-disable -->
-    <p class="animated fadeInRight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur minus reprehenderit commodi totam delectus aspernatur deserunt in, culpa veniam ea, ad itaque architecto exercitationem quasi tenetur quas, voluptas consequuntur ipsum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium aperiam voluptatem rerum incidunt veniam accusamus eius, saepe, aliquam consequuntur assumenda veritatis iusto dignissimos quibusdam officia eum, mollitia quo quae pariatur.</p>
+    <p class="animate__animated animate__fadeInRight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur minus reprehenderit commodi totam delectus aspernatur deserunt in, culpa veniam ea, ad itaque architecto exercitationem quasi tenetur quas, voluptas consequuntur ipsum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium aperiam voluptatem rerum incidunt veniam accusamus eius, saepe, aliquam consequuntur assumenda veritatis iusto dignissimos quibusdam officia eum, mollitia quo quae pariatur.</p>
     <FontAwesomeIcon icon="shopping-cart"></FontAwesomeIcon>
   </div>
 </template>
