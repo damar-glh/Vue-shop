3ab46607c63c47b813f21d02181c769aa548eff5
damara <damarmuhamad13@gmail.com>
2023-10-17 19:38:34 +0700

    add (Navbar): navbar-page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app" class="container mt-5">
-    <h1>Vue Shop</h1>
+  <div id="app" class="container">
+    <h1>IDShop</h1>
+    <navbar
+      :cart="cart"
+      :cartQty="cartQty"
+      :cartTotal="cartTotal"
+      @toggle="toggleSliderStatus()"
+      @delete="deleteItem()"
+    ></navbar>
     <price-slider
-      :sliderStatus="sliderStatus"
-      v-model:maximum.sync="maximum"
+      :sliderStatus="style.sliderStatus"
+      v-model:maximum="maximum"
     ></price-slider>
     <product-list
-      :products="products"
       :maximum="maximum"
-      @add="addItem()"
+      :products="products"
+      @add="addItem"
     ></product-list>
   </div>
 </template>
@@ -16,21 +23,24 @@
 <script>
 import ProductList from "./components/ProductList.vue";
 import PriceSlider from "./components/PriceSlider.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
-  name: "App",
+  name: "app",
   data: function () {
     return {
       maximum: 50,
       products: [],
       cart: [],
-      sliderStatus: true,
+      style: {
+        sliderStatus: false,
+      },
     };
   },
   components: {
-    // FontAwesomeIcon,
     ProductList,
     PriceSlider,
+    Navbar,
   },
   mounted: function () {
     fetch("https://hplussport.com/api/products/order/price")
@@ -39,7 +49,26 @@ export default {
         this.products = data;
       });
   },
+  computed: {
+    cartTotal: function () {
+      let sum = 0;
+      for (let key in this.cart) {
+        sum = sum + this.cart[key].product.price * this.cart[key].qty;
+      }
+      return sum;
+    },
+    cartQty: function () {
+      let qty = 0;
+      for (let key in this.cart) {
+        qty = qty + this.cart[key].qty;
+      }
+      return qty;
+    },
+  },
   methods: {
+    toggleSliderStatus: function () {
+      this.style.sliderStatus = !this.style.sliderStatus;
+    },
     addItem: function (product) {
       let productIndex;
       let productExist = this.cart.filter(function (item, index) {
@@ -50,10 +79,18 @@ export default {
           return false;
         }
       });
+
       if (productExist.length) {
         this.cart[productIndex].qty++;
       } else {
         this.cart.push({ product: product, qty: 1 });
+      }
+    },
+    deleteItem: function (id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
       }
     },
   },
